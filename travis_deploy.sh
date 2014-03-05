062756184beff12c558f9157781fe7b4fe038f75
Saurabh Kumar <thes.kumar@gmail.com>
2014-03-05 19:58:47 +0530

    fix travis deploy script

--- travis_deploy.sh
+++ travis_deploy.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 ghp-import ${OUTPUTDIR} -m "Blog updated with travis"
-git push --force --quiet "https://${GH_TOKEN}@${GH_REF}" master:gh-pages > /dev/null 2>&1
+git push --force --quiet "https://${GH_TOKEN}@${GH_REF}" gh-pages:gh-pages
 echo 'Deployed to gh-pages'
