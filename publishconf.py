eb41c858eea00a2eb6cc428086e8043e2e7776d1
Saurabh Kumar <me+github@saurabh-kumar.com>
2015-05-01 11:08:38 +0530

    fix(setup): update PYTHON_PATH for proper import
    - add travis build status badge

--- publishconf.py
+++ publishconf.py
@@ -1,16 +1,18 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*- #
-from __future__ import unicode_literals
-
-# This file is only used if you use `make publish` or
-# explicitly specify it as your config file.
+'''
+This file is only used if you use `make publish` or explicitly specify it as
+your config file.
+'''
+from __future__ import absolute_import, unicode_literals
 
 import os
 import sys
 
-from pelicanconf import *  # noqa
-
+# This must be before pelicanconf import
 sys.path.append(os.curdir)
+
+from pelicanconf import *  # noqa
 
 SITEURL = 'https://saurabh-kumar.com/blog'
 RELATIVE_URLS = True
