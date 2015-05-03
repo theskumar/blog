0d5b36123fde6cb03e9bfd6fdfaea6168cc91da0
Saurabh Kumar <me+github@saurabh-kumar.com>
2015-05-03 12:56:00 +0530

    Use absolute url in publish conf

--- publishconf.py
+++ publishconf.py
@@ -15,7 +15,7 @@ sys.path.append(os.curdir)
 from pelicanconf import *  # noqa
 
 SITEURL = 'https://saurabh-kumar.com/blog'
-RELATIVE_URLS = True
+RELATIVE_URLS = False
 
 FEED_ALL_ATOM = 'feeds/all.atom.xml'
 CATEGORY_FEED_ATOM = 'feeds/%s.atom.xml'
